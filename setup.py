d6637f8d08e4534e93ed3ce20c65a22661cf7f18
RADHA AGARWAL <radhaagrwal.380@gmail.com>
2025-11-14 12:54:29 +0530

    Fix README file reference

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from setuptools import setup
-with open("README.MD", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 AUTHOR_NAME = 'Radha Agarwal'
-SRC_REPO = 'src'
+SRC_REPO = 'movie-recommendation'
 LIST_OF_REQUIREMENTS = ['streamlit']
 
 setup(
-      name = SRC_REPO,
-    version = '1.0.0',
-    author = AUTHOR_NAME,
-    author_email= 'radhaagrwal.380@gmail.com',
-    description= 'Asmall example package for movie recommendation ',
-    long_description= long_description,
-    long_description_content_type= 'text/markdown',
-    package = [SRC_REPO],
-    python_requires= '>=3.7',
-    install_requires= LIST_OF_REQUIREMENTS ,
+    name=SRC_REPO,
+    version='1.0.0',
+    author=AUTHOR_NAME,
+    author_email='radhaagrwal.380@gmail.com',
+    description='A movie recommendation system',
+    long_description="A movie recommendation system using machine learning",
+    long_description_content_type='text/markdown',
+    packages=['src'],
+    package_dir={'src': 'src'},
+    python_requires='>=3.7',
+    install_requires=LIST_OF_REQUIREMENTS,
 )
-  
